b4e948af1e7e11e076d746ada6e699dcff309ba6
Dinesh B S <dineshbs444@gmail.com>
2021-07-18 23:15:00 +0530

    compiled, migrated to Rinkeby, verified smart contract on Etherscan, minted a new Token from Etherscan

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-// migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const CustomERC721Token = artifacts.require("./CustomERC721Token.sol");
+const SquareVerifier = artifacts.require("./Verifier.sol");
+const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+
+  deployer.deploy(CustomERC721Token);
+  deployer.deploy(SquareVerifier).then( () => {
+    return deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
+  });
 };
